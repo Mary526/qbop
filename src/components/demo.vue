e1a2a8a9967c16cecbd9ca53fbd1c3d4e9d9b61f
mary526 <mary26@icloud.com>
2021-08-14 21:35:14 +0800

    添加2021/8/14的日志

---
+++ src/components/demo.vue
@@ -0,0 +1,36 @@
+<template>
+    <canvas id="draw" width="752" height="752">Your browser can't support canvas tag</canvas>
+    
+</template>
+
+<script>
+    export default {
+        name: "demo",
+        mounted() {
+            this.init()
+        },
+        methods: {
+            init() {
+                let myDraw = document.getElementById("draw");
+                // if(myDraw.getContext) {
+
+                    let ctx = myDraw.getContext("2d");
+                    // context.fillStyle = "#f361ee";
+                    // context.fillRect(50, 50, 100, 100);
+                    // context.fillStyle = "rgba(0, 0, 255, 0.5)";
+                    // context.fillRect(100, 100, 100, 100);
+                let image = new Image()
+                image.src = 'https://debake.oss-cn-shanghai.aliyuncs.com/room/dantijiaju/di/jpg/Dtjj%20%281%29.jpg'
+                image.onload = ()=> {
+                    ctx.drawImage(image,0,0,752,752)
+                }
+                // }
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -16,10 +16,65 @@
             我们在武汉陪你一起看EVA[Cat_congrats]</p>
         <p>
             （欢迎小伙伴们进群一起助力活动～[Cat_heart]）</p>
-        <img src="../assets/qrcode.png" style="width: 100%;padding:9px;border: 1px solid #f0f0f0;"/>
+        <img src="../assets/qrcode.png" style="width: 96%;margin-left: 2%"/>
 
         <van-cell title="活动创建日志" icon="play" />
         <van-steps direction="vertical" :active="0">
+            <van-step>
+                <p>和影院方沟通，确认最终活动时间</p>
+                <p>活动时间:<b>2021/9/5（周日）15:00 PM</b></p>
+                <p>缴费报名截止时间：<b>2021/9/1 12:00 AM</b></p>
+                <p>欢迎大家踊跃报名～（准备二刷ing...</p>
+                <p>参考资料：<a href="https://mp.weixin.qq.com/s/CR_2krkj3VQEXaie-iEbPg">武汉连续两日0新增！中小学开学时间定了！湖北疾控发布紧急提示……</a></p>
+                <p>2021-08-15</p>
+            </van-step>
+            <van-step>
+                <p>亚马逊资源上线啦～感谢群友们热情提供资源链接，需要的朋友请参考下列链接哦：</p>
+                <p>1.<a href="https://pan.baidu.com/s/1BY-TFdDOsBior6fHlcFFxg">百度网盘版本-新·福音战士剧场版：终 シン・エヴァンゲリオン劇場版:│▌ (2021)</a> 提取码: yjnb</p>
+                <p>2.<a href="http://sharetxts.com/sharepage/6114d87caf32ec45fd62795d">汇总帖-新·福音战士剧场版：终 シン・エヴァンゲリオン劇場版:│▌ (2021)</a></p>
+                <p>3.<a href="https://www.aliyundrive.com/s/5CTvmgnNSwF">阿里云盘版本-新·福音战士剧场版：终 シン・エヴァンゲリオン劇場版:│▌ (2021)</a></p>
+
+
+                <p><b>PS: 特别感谢提供资源的群友:z****h、橘*、陈*</b></p>
+                <p>PPS : 资源仅供大家娱乐学习，请不要用于商业用途喔</p>
+                <p>2021-08-13</p>
+            </van-step>
+            <van-step>
+                <p>群内组织线上观影《EVA 序 1.11》</p>
+                <p>2021-08-08</p>
+            </van-step>
+            <van-step>
+                <p>此次活动预定的影院宣布暂时停业</p>
+                <p>我们的活动初步推迟至九月五号，下午三点，活动地点不变</p>
+                <p>有合适的建议（关于活动时间、活动地点等）也欢迎群内提出～</p>
+                <p>另：原则上，现在不鼓励大家缴费，有需求的朋友可以参照活动日志（2021/8/4）的信息办理退票。</p>
+                <p>又另：已经缴费的朋友，也请不要着急，【报名确认函】会在晚些时候发送至你的邮箱，请注意查收喔</p>
+                <p>希望，这会是你我一同度过的，非常难忘的夏天——我们曾直面疫情的邪恶与反复</p>
+                <p>2021-08-06</p>
+            </van-step>
+            <van-step>
+                <p>新增小伙伴两位</p>
+                <p>受疫情影响，进群问卷增加"入群原因"的输入框</p>
+                <p>2021-08-05</p>
+            </van-step>
+
+            <van-step>
+                <p>武汉开始全员核算检查，乡亲们请先别着急哇，王尔德还在积极观望、准备中——线下聚会一定会有的，EVA的美好记忆也一定会有的</p>
+                <p>“Hope is a good thing, maybe the best of things, and no good thing ever dies.”</p>
+                <p>PS : 着急退票的旁友，请通过邮件发送自己的收款码，礼貌的用语会更受欢迎。王尔德(<a href="mailto:greyson7@yeah.net">greyson7@yeah.net</a>)看到了会第一时间给你退款～</p>
+                <p>2021-08-04</p>
+            </van-step>
+            <van-step>
+                <p>新增1位小伙伴</p>
+                <p>May it be an evening star</p>
+                <p>Shines down upon you
+                </p>
+                <p>May it be when darkness falls
+                </p>
+                <p>Your heart will be true</p>
+                <p>删掉了发在EVA吧的宣传回帖</p>
+                <p>2021-08-01</p>
+            </van-step>
             <van-step>
                 <p>新增挺多小伙伴</p>
                 <p>自助缴费问卷增加了"备注"字段</p>
@@ -39,6 +94,8 @@
                 <p>活动时间：8/15 15:00</p>
                 <p>门票：六十</p>
                 <p>福音战士们，我们不说再见</p>
+                <p>影院场地已预定</p>
+                <p>在EVA吧回帖进行活动宣传</p>
                 <p>2021-07-28</p>
             </van-step>
             <van-step>
